dcaeec43037b7e153d201ad7c97994bbdd6017eb
Kamrul Hasan Shahed <khshahed90@gmail.com>
2018-07-27 12:15:40 +0600

    structure changed

--- design-patterns/chaining.js
+++ design-patterns/chaining.js
@@ -15,10 +15,4 @@ const Calc = function (start) {
   };
 };
 
-module.exports = () => new Calc(0)
-    .add(1)
-    .add(2)
-    .multiply(3)
-    .equals(function (result) {
-      console.log(result);
-    });
+module.exports = Calc;
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const chaining = require('./chaining');
+const chaining = require('./design-patterns/chaining');
 
-chaining();
+new chaining(0)
+    .add(1)
+    .add(2)
+    .multiply(3)
+    .equals(function (result) {
+      console.log(result);
+    });
